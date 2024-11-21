158dd3cbebd952711594d9e944d87dbf00f0d802
Rangga857 <azharrangga28@gmail.com>
2024-11-21 16:53:34 +0700

    create class RencanaStudyViewModel di folder ui/view/viewmodel

---
+++ app/src/main/java/com/example/navigationsomepage/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.navigationsomepage.ui.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.navigationsomepage.model.RencanaStudy
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class RencanaStudyViewModel : ViewModel() {
+
+}
