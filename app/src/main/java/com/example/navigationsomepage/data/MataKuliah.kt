574e617b3559f281e6d21e2fc6395183bf0b0a49
Rangga857 <azharrangga28@gmail.com>
2024-11-21 16:40:16 +0700

    add value optios yang dimiliki oleh variable options

--- app/src/main/java/com/example/navigationsomepage/data/MataKuliah.kt
+++ app/src/main/java/com/example/navigationsomepage/data/MataKuliah.kt
@@ -2,6 +2,11 @@ package com.example.navigationsomepage.data
 
 object MataKuliah {
     val options = listOf(
-
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision"
     )
 }
